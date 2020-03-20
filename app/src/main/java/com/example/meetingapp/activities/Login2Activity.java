51552e6cf2976c6613eb1a5cc3e482c9ebe79264
maxim <maximus1998g@mail.ru>
2020-03-20 13:41:18 +0300

    reset password

--- app/src/main/java/com/example/meetingapp/activities/Login2Activity.java
+++ app/src/main/java/com/example/meetingapp/activities/Login2Activity.java
@@ -3,18 +3,14 @@ package com.example.meetingapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.meetingapp.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -24,10 +20,12 @@ import java.util.Objects;
 public class Login2Activity extends AppCompatActivity {
 
     MaterialEditText email, password;
-    Button btn_register;
+    Button btn_login;
 
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
+
+    TextView forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,38 +36,32 @@ public class Login2Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
-        btn_register = findViewById(R.id.registerButton);
+        btn_login = findViewById(R.id.loginButton);
+        forgot_password = findViewById(R.id.forgot_password);
+        forgot_password.setOnClickListener(v -> startActivity(new Intent(Login2Activity.this, ResetPasswordActivity.class)));
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        btn_login.setOnClickListener(v -> {
+            String txt_email = Objects.requireNonNull(email.getText()).toString();
+            String txt_password = Objects.requireNonNull(password.getText()).toString();
 
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String txt_email = Objects.requireNonNull(email.getText()).toString();
-                String txt_password = Objects.requireNonNull(password.getText()).toString();
-
-                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password))
-                    Toast.makeText(Login2Activity.this, "Заполни все поля!", Toast.LENGTH_SHORT).show();
-                else
-                    firebaseAuth.signInWithEmailAndPassword(txt_email, txt_password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if(task.isSuccessful()){
-                                        Intent intent = new Intent(Login2Activity.this, Main2Activity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                    else {
-                                        Toast.makeText(Login2Activity.this, "Auth failed!", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-            }
+            if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password))
+                Toast.makeText(Login2Activity.this, "Заполни все поля!", Toast.LENGTH_SHORT).show();
+            else
+                firebaseAuth.signInWithEmailAndPassword(txt_email, txt_password)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(Login2Activity.this, Main2Activity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(Login2Activity.this, "Auth failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
         });
     }
 }
