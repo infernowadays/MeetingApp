ac5a75e0a60ef6ac48c4a3ea6ec53a25519fe6e3
maxim <maximus1998g@mail.ru>
2020-05-19 15:13:49 +0300

    trying to fix not showing options menu

--- app/src/main/java/com/example/meetingapp/fragments/ContentFragment.java
+++ app/src/main/java/com/example/meetingapp/fragments/ContentFragment.java
@@ -28,85 +28,85 @@ import butterknife.OnClick;
 
 public abstract class ContentFragment extends Fragment {
 
-    final int REQUEST_CODE = 1337;
-    @BindView(R.id.recycler_view)
-    RecyclerView recyclerView;
-    @BindView(R.id.floating_action_button)
-    FloatingActionButton floatingActionButton;
-    @BindView(R.id.swipe_layout)
-    SwipeRefreshLayout swipeRefreshLayout;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.toolbar_header)
-    TextView toolbarHeader;
-
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_content, container, false);
-        ButterKnife.bind(this, view);
-
-        setupToolbar();
-        loadContent(null);
-
-        swipeRefreshLayout.setOnRefreshListener(() -> {
-            loadContent(null);
-            swipeRefreshLayout.setRefreshing(false);
-        });
-
-
-        return view;
-    }
-
-    @OnClick(R.id.toolbar_header)
-    void changeContent() {
-        BottomSheetFragment bottomSheetFragment = BottomSheetFragment.newInstance();
-        bottomSheetFragment.show(requireActivity().getSupportFragmentManager(), BottomSheetFragment.TAG);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_filter:
-                openFilterDialog();
-                return true;
-            case R.id.action_search:
-                // do your code
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void setupToolbar() {
-        toolbar.setTitle("");
-        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
-
-        toolbarHeader.setText(getContentType());
-        int filterIcon = R.drawable.ic_expand_more_black_24dp;
-        toolbarHeader.setCompoundDrawablesWithIntrinsicBounds(0, 0, filterIcon, 0);
-
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (dy > 0 && floatingActionButton.getVisibility() == View.VISIBLE) {
-                    floatingActionButton.hide();
-                } else if (dy < 0 && floatingActionButton.getVisibility() != View.VISIBLE) {
-                    floatingActionButton.show();
-                }
-            }
-        });
-    }
-
-    public abstract void openFilterDialog();
-
-    public abstract void loadContent(List<String> categories);
-
-    public abstract String getContentType();
+//    final int REQUEST_CODE = 1337;
+//    @BindView(R.id.recycler_view)
+//    RecyclerView recyclerView;
+//    @BindView(R.id.floating_action_button)
+//    FloatingActionButton floatingActionButton;
+//    @BindView(R.id.swipe_layout)
+//    SwipeRefreshLayout swipeRefreshLayout;
+//    @BindView(R.id.toolbar)
+//    Toolbar toolbar;
+//    @BindView(R.id.toolbar_header)
+//    TextView toolbarHeader;
+//
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_content, container, false);
+//        ButterKnife.bind(this, view);
+//
+//        setupToolbar();
+//        loadContent(null);
+//
+//        swipeRefreshLayout.setOnRefreshListener(() -> {
+//            loadContent(null);
+//            swipeRefreshLayout.setRefreshing(false);
+//        });
+//
+//
+//        return view;
+//    }
+//
+//    @OnClick(R.id.toolbar_header)
+//    void changeContent() {
+//        BottomSheetFragment bottomSheetFragment = BottomSheetFragment.newInstance();
+//        bottomSheetFragment.show(requireActivity().getSupportFragmentManager(), BottomSheetFragment.TAG);
+//    }
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_filter:
+//                openFilterDialog();
+//                return true;
+//            case R.id.action_search:
+//                // do your code
+//                return true;
+//
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+//    private void setupToolbar() {
+//        toolbar.setTitle("");
+//        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+//
+//        toolbarHeader.setText(getContentType());
+//        int filterIcon = R.drawable.ic_expand_more_black_24dp;
+//        toolbarHeader.setCompoundDrawablesWithIntrinsicBounds(0, 0, filterIcon, 0);
+//
+//        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//                if (dy > 0 && floatingActionButton.getVisibility() == View.VISIBLE) {
+//                    floatingActionButton.hide();
+//                } else if (dy < 0 && floatingActionButton.getVisibility() != View.VISIBLE) {
+//                    floatingActionButton.show();
+//                }
+//            }
+//        });
+//    }
+//
+//    public abstract void openFilterDialog();
+//
+//    public abstract void loadContent(List<String> categories);
+//
+//    public abstract String getContentType();
 }
