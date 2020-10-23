1c57a538625f43bd82fe7d60f9dae57a96fd04f9
Mortd3kay <mortd3kay@users.noreply.github.com>
2020-10-23 16:36:10 +0300

    появляется кропер, с камеры изображение пиксельное, вылетает при переходе в основной фрагмент

---
+++ app/src/main/java/com/example/meetingapp/activities/CropImageActivity.java
@@ -0,0 +1,108 @@
+package com.example.meetingapp.activities;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
+import butterknife.BindView;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.system.Os;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.meetingapp.R;
+import com.example.meetingapp.fragments.profile_stepper.UserBasicInformationStepperFragmentFragment;
+import com.steelkiwi.cropiwa.CropIwaView;
+import com.steelkiwi.cropiwa.config.CropIwaSaveConfig;
+
+import java.io.File;
+import java.util.Objects;
+
+public class CropImageActivity extends AppCompatActivity {
+
+    CropIwaView cropView;
+    ImageView crop_btn;
+    int code;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_image);
+
+        cropView = (CropIwaView) findViewById(R.id.crop_view);
+        crop_btn = (ImageView) findViewById(R.id.crop_btn);
+
+        crop_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Не уверен, что это удачный uri
+
+                Uri resourceURI = Uri.parse("android.resource://" + getPackageName() + "/cropped.png");
+
+                cropView.crop(new CropIwaSaveConfig.Builder(resourceURI)
+                        .setCompressFormat(Bitmap.CompressFormat.PNG)
+                        .setQuality(100)
+                        .build());
+                Intent cropped = new Intent();
+                System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+resourceURI.toString());
+                cropped.putExtra("uri", resourceURI.toString());
+                setResult(Activity.RESULT_OK, cropped);
+                finish();
+            }
+        });
+
+        Bundle bundle = getIntent().getExtras();
+
+        assert bundle != null;
+        code = bundle.getInt("request");
+
+        if (code == UserBasicInformationStepperFragmentFragment.IMAGE_REQUEST){
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, code);
+        } else if (code == UserBasicInformationStepperFragmentFragment.CAMERA_REQUEST){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, code);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == code && resultCode == Activity.RESULT_OK && data != null){
+            if (code == UserBasicInformationStepperFragmentFragment.IMAGE_REQUEST) {
+                Uri imageUri = data.getData();
+                cropView.setImage(BitmapFactory.decodeFile(getRealPathFromUri(imageUri)));
+            } else if (code == UserBasicInformationStepperFragmentFragment.CAMERA_REQUEST){
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                cropView.setImage(photo);
+            }
+        } else {
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+        }
+    }
+
+    private String getRealPathFromUri(Uri contentUri) {
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(this, contentUri, filePathColumn, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = Objects.requireNonNull(cursor).getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+}
--- app/src/main/java/com/example/meetingapp/fragments/profile_stepper/UserBasicInformationStepperFragmentFragment.java
+++ app/src/main/java/com/example/meetingapp/fragments/profile_stepper/UserBasicInformationStepperFragmentFragment.java
@@ -3,8 +3,10 @@ package com.example.meetingapp.fragments.profile_stepper;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -32,6 +34,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.loader.content.CursorLoader;
 
+import com.example.meetingapp.activities.CropImageActivity;
 import com.example.meetingapp.utils.images.compression.Compressor;
 import com.example.meetingapp.utils.images.DownloadImageTask;
 import com.example.meetingapp.interfaces.GetImageFromAsync;
@@ -73,7 +76,9 @@ import static android.app.Activity.RESULT_OK;
 public class UserBasicInformationStepperFragmentFragment extends Fragment implements BlockingStep,
         DatePickerDialog.OnDateSetListener, GetImageFromAsync {
 
-    private static final int IMAGE_REQUEST = 100;
+    public static final int IMAGE_REQUEST = 100;
+    public static final int CAMERA_REQUEST = 101;
+    private static final int CROP_REQUEST = 102;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -154,11 +159,36 @@ public class UserBasicInformationStepperFragmentFragment extends Fragment implem
     void openImage() {
         verifyStoragePermissions(requireActivity());
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(requireActivity(),
-                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, IMAGE_REQUEST);
-        }
+        selectImage();
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(requireActivity(),
+//                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            startActivityForResult(intent, IMAGE_REQUEST);
+//        }
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                Intent intent = new Intent(getContext(), CropImageActivity.class);
+                if (items[item].equals("Take Photo")) {
+                    intent.putExtra("request", CAMERA_REQUEST);
+                    startActivityForResult(intent, CROP_REQUEST);
+                } else if (items[item].equals("Choose from Library")) {
+                    intent.putExtra("request", IMAGE_REQUEST);
+                    startActivityForResult(intent, CROP_REQUEST);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 
     private String getRealPathFromUri(Uri contentUri) {
@@ -175,9 +205,10 @@ public class UserBasicInformationStepperFragmentFragment extends Fragment implem
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
-            Uri imageUri = data.getData();
-            upload(imageUri);
+        if (requestCode == CROP_REQUEST && resultCode == RESULT_OK && data != null) {
+            Bundle bundle = data.getExtras();
+            Uri imageUri = Uri.parse((String)bundle.get("uri"));
+            imageProfile.setImageURI(imageUri);
         }
     }
 
